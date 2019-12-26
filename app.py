74eaf808c75581076ce7d00af85fee8637d63f5d
serinir <imad-eddine.merzoug@etu.usthb.dz>
2019-12-26 22:21:40 +0100

    voici voila

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from http import server,HTTPStatus
+import sys
 #handler c'est comment on va servire , heda \
 # howa le plus simple eyserbess le fichier courrent direct
 handler = server.SimpleHTTPRequestHandler
-address = '127.0.0.1/vidal'
-port = 8000
+address = '127.0.0.1'
+port = int(sys.argv[1])
 #tuple fiha l'addres en premier puis le port
 addr_port = (address,port)
 # heda howa le serveur http 
