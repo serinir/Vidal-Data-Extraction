cd9b61cc8f1938ee239e3f220512d174c03a54d1
serinir <imad-eddine.merzoug@etu.usthb.dz>
2019-12-26 18:35:15 +0100

    premier commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from http import server,HTTPStatus
+#handler c'est comment on va servire , heda \
+# howa le plus simple eyserbess le fichier courrent direct
+handler = server.SimpleHTTPRequestHandler
+address = '127.0.0.1'
+port = 8000
+#tuple fiha l'addres en premier puis le port
+addr_port = (address,port)
+# heda howa le serveur http 
+httpd = server.HTTPServer(addr_port,handler)
+# hna pour lui dir serbess a wlidi
+httpd.serve_forever()
