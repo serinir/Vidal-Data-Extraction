9f760fe7906fd85da74579a88f9ea1d23f692463
serinir <imad-eddine.merzoug@etu.usthb.dz>
2020-01-04 19:25:09 +0100

    voila voila

--- aspirer.py
+++ aspirer.py
@@ -8,10 +8,9 @@ def illgetagoodlookingnamesoon(debut : str,fin :str) :
     for i in range(ord(debut.capitalize())+1,ord(fin.capitalize())+1):
         retour.append(chr(i))
     return retour
-
 #Port & intervale de lettres
 port =  sys.argv[2]
-inter = sys.argv[1].split('-',1)
+inter = sys.argv[1].upper().split('-',1)
 #source /le lien de debut etc etc
 src = 'http://localhost:{}'.format(port) 
 #warning au cas ou les parametres mechi cheban
@@ -22,20 +21,29 @@ if inter[0].capitalize() > inter[1].capitalize() :
     warning("bounds error!")
     exit()
 letters = illgetagoodlookingnamesoon(inter[0],inter[1])
+# le format de nos pages
 pages = ['{}/vidal/vidal-Sommaires-Substances-{}.htm'.format(src,i) for i in letters]
+# ici on va mettre nos data netoyer a.k.a le noms des medicament
 data_cleaned=[]
+# boucle de scraping par page
 for url in pages:
+    #on fait une requete
     with requests.get(url) as p:
+        # on precise l'encodage
         p.encoding='utf-8'
+        # le workflow de beautifulsoup , lire l'html/chercher l'element / prendre la donné en text
         soup = BeautifulSoup(p.text,'html.parser')
         soup = soup.find('ul',class_='substances list_index has_children')
         data = soup.findAll('li')
+        # mettre le tout dans data cleand
         data_cleaned.extend([i.text.strip() for i in data])
+
 if __name__ == "__main__":
-    print("yalkiwi")
+    # on ecirs dans subst.dic
     with open('subst.dic','w',encoding='utf-16-le') as f:
             for i in data_cleaned :
                 f.write(i+','+'.N+subst\n')
+    # on ecris dans info.txt
     with open('info.txt','w') as file :
         lettersDico = {}
         k=0
@@ -46,7 +54,4 @@ if __name__ == "__main__":
             k+=len(lettersDico[i])
             file.write('{}:{}\n'.format(i.upper(),len(lettersDico[i])))
         file.write("Total:{}".format(len(data_cleaned)))
-        print(k)
     print(len(data_cleaned))
-    a={'z':'b'}
-    
--- enrechi.py
+++ enrechi.py
@@ -1,11 +1,12 @@
 import sys,re
-
+#les expression reguliaires
 regex1 = r"([a-zA-ZÉé]{5,}?)(?=\s*(K|LP|IVD)?\s*(\d+((,|\.)?\d+)?|½)\s*(MG|ML|G|mL|GR|µg|mg|mcg|UI|mG|mL|ml|g|gr|l|L|unités|cuillère à soupe|sachet|sachets|cps|flacon|sachet|cp|cps|sachet|sachets|injection|seringue|amp|IVL)?\W*:\s*(\d|½))"
 regex2 = r"([a-zA-ZÉé]{5,}?)(?=\s*(K|LP|IVD)?\s*(\d+((,|\.)?\d+)?|½)\s*(MG|ML|G|mL|GR|µg|mg|mcg|UI|mG|mL|ml|g|gr|l|L|unités|cuillère à soupe|sachet|sachets|cps|flacon|sachet|cp|cps|sachet|sachets|injection|seringue|amp|IVL),\s*)"
 regex3 = r"^-? ?([a-zA-ZÉé]{5,}) :? ?(\d+|,)+ (MG|ML|G|mL|GR|µg|mg|mcg|UI|mG|mL|ml|g|gr|l|L|unités|cuillère à soupe|sachet|sachets|cps|flacon|sachet|cp|cps|sachet|sachets|injection|seringue|amp|IVL).+"
 regex4 = r"^Ø? ?([a-zA-ZÉé]{5,}) :? ?(\d+|,)+ (MG|ML|G|mL|GR|µg|mg|mcg|UI|mG|mL|ml|g|gr|l|L|unités|cuillère à soupe|sachet|sachets|cps|flacon|sachet|cp|cps|sachet|sachets|injection|seringue|amp|IVL).+"
 medoc =[]
 j=0
+#on parse le fichier en argument et on met les match dans la list medoc
 with open(sys.argv[1]) as f :
     for i in f.readlines() :
         r =  re.search(regex1,i)
@@ -32,7 +33,9 @@ with open(sys.argv[1]) as f :
             resp = str(r.group(1)).upper()
             if resp not in medoc :
                 medoc.append(resp)
+# trie de la liste
 medoc.sort()
+# on supprime les mots non medicament qui sont passer dans la regex
 medoc.pop(medoc.index('JANVIER'))
 medoc.pop(medoc.index('DIFFU'))
 medoc.pop(medoc.index('PROBABILISTE'))
@@ -43,13 +46,17 @@ medoc.pop(medoc.index('POSOLOGIE'))
 medoc.pop(medoc.index('RETARD'))
 medoc.pop(medoc.index('INTRAVEINEUSE'))
 medoc.pop(medoc.index('POTASSIUM'))
+
 data_cleaned = []
+# on lit notre subst.dic
 with open('subst.dic','r',encoding='utf-16-le') as f :
     for i in f.readlines():
         data_cleaned.append(i.split(',')[0])
+# on ecris notre subst-enri.dic
 with open('subst-enri.dic','w',encoding='utf-16-le') as f :
     for i in medoc:
         f.write("{},.N+subst\n".format(i))
+# on ecris notre info2.txt
 with open('info2.txt','w') as file :
         lettersDico = {}
         k=0
@@ -60,12 +67,13 @@ with open('info2.txt','w') as file :
             k+=len(lettersDico[i])
             file.write('{}:{}\n'.format(i.upper(),len(lettersDico[i])))
         file.write("Total:{}".format(len(medoc)))
-
+# on enrichi nos medoc
 for i in data_cleaned : 
     if i not in medoc :
         medoc.append(i)
 medoc.sort()
+# on réecris dans subst.dic pour avoir la totalité des medicament 
 with open('subst.dic','w',encoding='utf-16-le') as f :
     for i in medoc:
         f.write("{},.N+subst\n".format(i))
-print(len(medoc))  
+print(len(medoc)) 
